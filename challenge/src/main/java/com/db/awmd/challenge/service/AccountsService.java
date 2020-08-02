be1c9a7c7c6c9ee7455b9d9f496a6dc047a5b8e2
prem0903 <69111537+prem0903@users.noreply.github.com>
2020-08-02 22:57:53 +0530

    Update AccountsService.java

--- challenge/src/main/java/com/db/awmd/challenge/service/AccountsService.java
+++ challenge/src/main/java/com/db/awmd/challenge/service/AccountsService.java
@@ -6,6 +6,7 @@ import com.db.awmd.challenge.repository.AccountsRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
